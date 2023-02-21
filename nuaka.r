5157a6bd3e4894e27e56839be337eb9bf9b7b968
MarcinSolnicki <marcinsolnicki@gmail.com>
2023-02-21 17:01:36 +0100

    Delete nuaka.r

--- nuaka.r
+++
@@ -1,9 +0,0 @@
-# Marcin Solnicki 122816
-readMap<-function(path){
-data <- read.delim(file = path,header = TRUE, sep = "\t", dec = ".")#it will be data.frame
-return(data)
-}
-print(readMap("C:/Users/soma2003/Downloads/maze_0.map"))
-findWay<-function(m){
-
-}
